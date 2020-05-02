99baaeb8a51a7d83af4c76a4060b5d349ea869c5
OlexandrKukhar <45728829+RedlineAlexander@users.noreply.github.com>
2020-05-02 15:04:49 +0300

    Fix#1

--- MarketBazarKukharITEAFinallyProject/Models/Entities/Buyers.cs
+++ MarketBazarKukharITEAFinallyProject/Models/Entities/Buyers.cs
@@ -12,8 +12,11 @@ namespace MarketBazarKukharITEAFinallyProject.Models.Entities
         [Key] public int BuyersId { get; set; }
         public string BuyersName { get; set; }
         public string BuyersGoodBuy { get; set; }
+        public List<Consumers> Consumers { get; set; }
         public int Id { get; set; }
         public DateTime? Sale { get; set; }
         public DateTime? LastSale { get; set; }
+
+
     }
 }
--- MarketBazarKukharITEAFinallyProject/Models/Entities/Consumers.cs
+++ MarketBazarKukharITEAFinallyProject/Models/Entities/Consumers.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace MarketBazarKukharITEAFinallyProject.Models.Entities
         public string ConsumerGood { get; set; }
         public string ConsumerPavilyon { get; set; }
         public int Id { get; set; }
+        [ForeignKey("BuyersId")] public Buyers Buyers { get; set; }
         public DateTime? Sale { get; set; }
         public DateTime? LastSale { get; set; }
     }
